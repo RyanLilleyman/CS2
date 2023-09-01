dc65bf81ce1be5b5d8a1e9928add228649e26565
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-09-01 18:58:27 -0400

    Add changes to course information.py

--- Chp9/exercises/courseInformation.py
+++ Chp9/exercises/courseInformation.py
@@ -1,13 +1,31 @@
 def main():
     n = int(input("How many classees are you taking this semester?"))
+    classes = {}
+    teachers = {}
+    meetingTimes = {}
     for i in range(n):
-        classes = {}
         print(f"Enter the {i}th course:")
         course = str(input())
+
         print(f"Enter the {course} room number")
         courseNumber = str(input())
 
+        print(f"Who is the professor of {course}?")
+        prof = str(input())
+
+        print(f"What is the meeting time of {course}?")
+        time = str(input())
+        teachers[course] = prof
+        meetingTimes[course] = time
         classes[course] = courseNumber
+
+    for k, v in classes.items():
+        print(k, v)
+
+    for k, v in teachers.items():
+        print(k, v)
+    for k, v in meetingTimes.items():
+        print(k, v)
 
 
 if __name__ == "__main__":
