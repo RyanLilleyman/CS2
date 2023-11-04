0a9ff13dda49724c7774a220523e158b3220c24f
Ryan Liilleyman <lilleymanryan2020@gmail.com>
2023-11-04 17:08:31 -0400

    added docstring

--- projects/project3/car.py
+++ projects/project3/car.py
@@ -1,7 +1,18 @@
 class Car:
+    """
+    A class representing a car.
+
+    Attributes:
+        brand (str): The brand of the car.
+        model (str): The model of the car.
+        year (int): The year the car was manufactured.
+        price (float): The price of the car.
+        car_type (str): The type of the car.
+    """
+
     def __init__(self, brand: str, model: str, year: int, price: float, car_type: str):
         self.brand = brand
         self.model = model
         self.year = year
         self.price = price
-        self.Type = Type
+        self.car_type = car_type
