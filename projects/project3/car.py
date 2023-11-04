39cefb45efe9122b672c43e5c937e551e9d85933
Ryan Liilleyman <lilleymanryan2020@gmail.com>
2023-11-04 17:03:45 -0400

    Added main project3 structure

---
+++ projects/project3/car.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self, brand: str, model: str, year: int, price: float, car_type: str):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.price = price
+        self.Type = Type
