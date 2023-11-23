8cc90bed467e38e8df82234243b1f22ec45aeb82
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-11-23 11:13:26 -0500

    Added comments

--- projects/project1/student.py
+++ projects/project1/student.py
@@ -5,23 +5,46 @@
 
 
 from typing import Tuple
-from typing_extensions import List
+
+# from typing_extensions import List
 
 
 class Student:
     def __init__(self, name: str, id: int):
+        """
+        Initializes a new instance of the class with the specified name and ID.
+
+        Parameters:
+            name (str): The name of the instance.
+            id (int): The ID of the instance.
+
+        Returns:
+            None
+        """
         self.__name = name
         self.__credits = 0
         self.__id = id
-        self.__course_list: List[Tuple] = []
+        self.__course_list = []
 
     def get_id(self):
+        """
+        Returns the ID of the object.
+        """
         return self.__id
 
     def get_name(self):
+        """
+        Return the name of the object.
+        """
         return self.__name
 
     def getCourseList(self):
+        """
+        Get the course list.
+
+        Returns:
+            list: The course list.
+        """
         return self.__course_list
 
     def getCredits(self):
